04ecccd4c0587580a59da9040cc3491f44183c08
Uros Isakovic <uros.isakovic98@gmail.com>
2019-11-14 19:51:32 +0100

    Implemented basic utility functions in imagelib.py

--- imagelib.py
+++ imagelib.py
@@ -1,14 +1,22 @@
 import numpy as np
 from PIL import Image
 
-def imopen():
-    pass
 
-def imwrite():
-    pass
+def imopen(img_path):
+    img = Image.open(img_path)
+    img = np.array(img, dtype=np.float32)
+    return img
 
-def rgb2gray():
-    pass
+def imwrite(img_path, img):
+    img = ((img - img.min()) / (img.max() - img.min())) * 255.
+    img = Image.fromarray(img.astype(np.uint8))
+    img.save(img_path)
+
+def rgb2gray(rgb, conv_ratio=None):
+    if conv_ratio is None:
+        conv_ratio = np.array([0.2989, 0.5870, 0.1140])
+
+    return np.dot(rgb, conv_ratio)
 
 def imblur_mean():
     pass
@@ -40,11 +48,18 @@ def histspec():
 def imsharpen():
     pass
 
-def sobelver_filter():
-    pass
-
-def sobelhor_filter():
-    pass
+def sobel_filter(type):
+    filter = np.array([[1, 0, -1],
+                       [2, 0, -2],
+                       [1, 0, -1]])
+    if type == 'ver':
+        return filter
+    elif type == 'hor':
+        return np.transpose(filter)
+        
 
 def laplassian_filter():
-    pass
+    filter = np.array([[ 0, -1,  0],
+                       [-1,  4, -1],
+                       [ 0, -1,  0]])
+    return filter
