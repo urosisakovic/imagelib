ab9651c8fb8eb8b6585b560bbb2acd28bb18b159
Uros Isakovic <uros.isakovic98@gmail.com>
2019-11-14 19:40:30 +0100

    Started formatting, debugging and benchmarking currently written code. Will be written in imagelib.py

---
+++ imagelib.py
@@ -0,0 +1,50 @@
+import numpy as np
+from PIL import Image
+
+def imopen():
+    pass
+
+def imwrite():
+    pass
+
+def rgb2gray():
+    pass
+
+def imblur_mean():
+    pass
+
+def imblur_gaussian():
+    pass
+
+def imblur_median():
+    pass
+
+def imresize():
+    pass
+
+def imaffine():
+    pass
+
+def filter():
+    pass
+
+def canny_edge():
+    pass
+
+def histeq():
+    pass
+
+def histspec():
+    pass
+
+def imsharpen():
+    pass
+
+def sobelver_filter():
+    pass
+
+def sobelhor_filter():
+    pass
+
+def laplassian_filter():
+    pass
