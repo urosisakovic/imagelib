f1ddd8cb2d8ed7c4158c313af815a76451965cca
Uros Isakovic <uros.isakovic98@gmail.com>
2019-11-15 20:24:54 +0100

    Adding comments.

--- imagelib.py
+++ imagelib.py
@@ -3,22 +3,43 @@ from PIL import Image
 
 
 def imopen(img_path):
+    """
+    Given filepath to an image, loads it as an numpy.ndarray of type float32.
+    """
     img = Image.open(img_path)
     img = np.array(img, dtype=np.float32)
     return img
 
 def imwrite(img_path, img):
+    """
+    Given filepath and an image in numpy.ndarray format, this functions stores it.
+
+    img_path(string): Filepath to a desired location.
+    img(numpy.ndarray): RGB or grayscale image.
+    """
     img = ((img - img.min()) / (img.max() - img.min())) * 255.
     img = Image.fromarray(img.astype(np.uint8))
     img.save(img_path)
 
+# TODO: Extend this to cover RGBA.
 def rgb2gray(rgb, conv_ratio=None):
+    """
+    Converts rgb image to grayscale.
+
+    rgb(numpy.ndarray): RGB image with shape [<height>, <width>, 3].
+    conv_ration(numpy.ndarray): Denotes how much green, red or blue affect
+        intensity grayscale image. Shape [1, 3]. Form [red_contrib, green_contrib, blue_contrib].
+
+    """
     if conv_ratio is None:
         conv_ratio = np.array([0.2989, 0.5870, 0.1140])
 
     return np.dot(rgb, conv_ratio)
 
 def imblur_mean(img, filt_shape):
+    """
+    Mean blur of an image.
+    """
     filt = mean_blur_filter(filt_shape)
     return filter2(img, filt)
 
@@ -247,4 +268,10 @@ def affine_transform():
 
 # TODO: copy syntax of cv2
 def threshold():
+    pass
+
+def rgb2hsv():
+    pass
+
+def hsv2rgb():
     pass
